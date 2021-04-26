fc31e6bb0bab87b601fadbcc0cae61a3c71a7518
kureharyosuke <ohhayo.com@gmail.com>
2021-04-26 11:53:30 +0900

    fix*: tailwindcss & storybook config / add: .eslintignore .eslintrc.json

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
-  stories: ['../src/components/**/*.stories.mdx', '../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/components/**/*.stories.mdx',
+    '../src/components/**/*.stories.@(js|jsx|ts|tsx)'
+  ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials']
 };
---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+export interface ButtonProps {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: 'small' | 'medium' | 'large';
+  label: string;
+  onClick?: () => void;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}) => {
+  const baseButton = 'rounded-full font-bold';
+  const sizeMode =
+    size === 'small'
+      ? 'py-1.5 px-4 text-xs'
+      : size === 'medium'
+      ? 'py-2 px-5 text-sm'
+      : size === 'large'
+      ? 'py-3 px-6 text-base'
+      : '';
+  return primary ? (
+    <div>
+      <button
+        type="button"
+        className={`text-white bg-gray-600 ${baseButton} ${sizeMode}`}
+        {...props}>
+        {label}
+      </button>
+    </div>
+  ) : (
+    <button
+      type="button"
+      className={`text-gray-600 bg-transparent bg-pink-500 shadow-inner ${baseButton} ${sizeMode}`}
+      style={{ backgroundColor }}
+      {...props}>
+      {label}
+    </button>
+  );
+};
